c96845c573617bc3ed9b8c5d61a11470bd9f85eb
kcrg <kacper@tryniecki.com>
2019-05-11 22:06:56 +0200

    Initial commit

---
+++ GustoUWP/Views/NavigationPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace GustoUWP.Views
+{
+    public sealed partial class NavigationPage : Page
+    {
+        public NavigationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
